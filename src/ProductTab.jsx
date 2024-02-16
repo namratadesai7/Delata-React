4a074c9ebe2d7d412e3bfc98d684f36beac2527e
Namrata Desai <namratadesai003@gmail.com>
2024-02-16 17:34:42 +0530

    React Props

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -3,11 +3,15 @@ import Product from "./product.jsx";
 
 
 function ProductTab() {
+    let options=["hit-tech","durable","fast"]; //array
+    let options2= {a: "hi-tech",b: "durable",c: "fast"}; //object
+    let options3=[<li>"hi-tech"</li>,<li>"durable"</li>,<li>"fast"</li>]
     return(
         <>
-        < Product  />
-        < Product  />
-        < Product  />
+        < Product title="phone" price={30000} features={options} features2={options2} 
+        features3={{a:"hi-tech"}} feats={options3} />
+        {/* < Product title="laptop" price="40,000" />
+        < Product title="pen"  /> */}
         </>
     );
 }
--- src/product.jsx
+++ src/product.jsx
@@ -1,11 +1,15 @@
 import "./product.css";
 
 
-function Product(){
+function Product({title,price=1,features,features2,features3,feats}){
+  //  const list= feats.map((feat) => <li>{feat}</li> )
     return(
         <div className="Product">
-            <h3>Product Title</h3>
-            <h5>Product Description</h5>
+            <h3>{title}</h3>
+            <h3>Price:{price}</h3>
+            {/* <h5>{features.a}</h5>
+            <h5>{features3.a}</h5> */}
+            <h5>{  feats.map((feat) => <li>{feat}</li> )}</h5>
         </div>
 
     )
