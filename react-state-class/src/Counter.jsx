16db88c6c2f224b7f763671e371f1664887f1db2
Namrata Desai <namratadesai003@gmail.com>
2024-02-20 19:24:13 +0530

    conditionals,Reacr dev tools,clicl events,states and Hooks

---
+++ react-state-class/src/Counter.jsx
@@ -0,0 +1,28 @@
+import {useState} from "react";
+
+export default function Counter(){
+   //let [stateVariable, setStateVariable]= useState(0);
+   let [count,setCount]= useState(0);
+   
+   let incCount=()=>{
+    setCount(count+1);
+    //console.log(count);
+   };
+
+    //the below function does not increase the count variable value in the component
+    // let count =0;
+
+    // function inCount(){
+    //     count +=1;
+    //     console.log(count);
+    // }
+    return (
+        <div>
+            
+            {/* <h3>Count={count}</h3>
+            <button onClick={inCount}>Increase Count</button> */}
+            <h3>Count={count} </h3>
+            <button onClick={incCount} >Increase Count</button>
+        </div>
+    )
+}
