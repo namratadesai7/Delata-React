9d94b5964c848be229f0bcb890f7b6c0ba703a89
Namrata Desai <namratadesai003@gmail.com>
2024-02-24 11:36:32 +0530

    react states

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import Counter from './Counter';
 import LikeButton from '../LikeButton';
+import LudoBoard from './LudoBoard';
 
 function App() {
 
@@ -9,8 +10,9 @@ function App() {
   return (
     <>
     <h1>States in React</h1>
-    <Counter />
+    {/* <Counter /> */}
     {/* <LikeButton /> */}
+    < LudoBoard />
     </>
   )
 }
