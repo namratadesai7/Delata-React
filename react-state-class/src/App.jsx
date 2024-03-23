bc7a39409fe9cdca94e5ffa672896df60835bbd0
Namrata Desai <namratadesai003@gmail.com>
2024-03-23 23:54:11 +0530

    todolist

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -3,16 +3,18 @@ import './App.css';
 import Counter from './Counter';
 import LikeButton from '../LikeButton';
 import LudoBoard from './LudoBoard';
+import TodoList from './TodoList';
 
 function App() {
 
 
   return (
     <>
-    <h1>States in React</h1>
+    {/* <h1>States in React</h1> */}
     {/* <Counter /> */}
     {/* <LikeButton /> */}
-    < LudoBoard />
+    {/* < LudoBoard /> */}
+    <TodoList />
     </>
   )
 }
